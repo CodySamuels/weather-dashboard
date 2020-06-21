80ddf482134028942339f76d51f5ac797cdb5f7b
Cody Samuels <twowoodenspoons@gmail.com>
2020-06-21 13:34:42 -0700

    HTML finished with placeholder info

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+// IDS TO KNOW
+
+// searchBar
+// searchButton
+// cityList
+// cityTemperature
+// cityHumidity
+// cityWindSpeed
+// cityUVIndex
+// card1Date
+// card1Temp
+// card1Humidity
+// card2Date
+// card2Temp
+// card2Humidity
+// card3Date
+// card3Temp
+// card3Humidity
+// card4Date
+// card4Temp
+// card4Humidity
+// card5Date
+// card5Temp
+// card5Humidity
