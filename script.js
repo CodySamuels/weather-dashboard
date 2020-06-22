e1ee9ef2b917aac56c6aaa4b036fc4be1c23d7a7
Cody Samuels <twowoodenspoons@gmail.com>
2020-06-22 16:18:14 -0700

    Feature complete. Need a sturdy readme. and a few edge cases fixed

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function renderTables(cityName) {
 
     $("#weatherStatsElement").show()
     $("#forecastStatsElement").show()
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
-            
+
             var cityLat = response.city.coord.lat;
             var cityLong = response.city.coord.lon;
             queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLong + "&units=imperial&exclude=minutely,hourly&appid=" + APIKey
@@ -32,13 +32,27 @@ function renderTables(cityName) {
                 .then(function (response) {
 
                     var currentDate = moment().format("MMM Do, YYYY");
+                    var uviInt = parseInt(response.current.uvi)
 
                     $("#cityHeaderDate").text("(" + currentDate + ")");
                     $("#cityHeaderImg").attr("src", "http://openweathermap.org/img/wn/" + (response.current.weather[0].icon) + ".png");
                     $("#cityTemperature").text("Temperature: " + response.current.temp + " °F");
                     $("#cityHumidity").text("Humidity: " + response.current.humidity + "%");
                     $("#cityWindSpeed").text("Wind Speed: " + response.current.wind_speed + " mph");
-                    $("#cityUVIndex").text("UV Index: " + response.current.uvi);
+                    $("#cityUVIButton").text(response.current.uvi);
+                    $("#cityUVIButton").removeClass();
+
+                    if (uviInt <= 2) {
+                        $("#cityUVIButton").addClass("greenlowUVIButton btn");
+                    } else if (uviInt > 2 && uviInt <= 5) {
+                        $("#cityUVIButton").addClass("yellowModerateUVIButton btn");
+                    } else if (uviInt > 5 && uviInt <= 7) {
+                        $("#cityUVIButton").addClass("orangeHighUVIButton btn");
+                    } else if (uviInt > 7 && uviInt <= 10) {
+                        $("#cityUVIButton").addClass("redVeryHighUVIButton btn");
+                    } else if (uviInt >= 11){
+                        $("#cityUVIButton").addClass("violetExtremeUVIButton btn");
+                    };
 
                     for (let i = 0, x = 1; i <= 4; i++, x++) {
                         currentDate = moment().add(x, 'days').format("MM/DD/YYYY")
@@ -48,10 +62,10 @@ function renderTables(cityName) {
                         $("#card" + i + "Temperature").text("Temperature: " + response.daily[i].temp.day + " °F");
                         $("#card" + i + "Humidity").text("Humidity: " + response.daily[i].humidity + "%");
                         $("#card" + i + "Img").attr("src", "http://openweathermap.org/img/wn/" + imgToAttach + ".png");
-                    }
+                    };
                 });
         });
-}
+};
 
 function renderSideBar() {
 
@@ -72,14 +86,14 @@ function renderSideBar() {
             renderTables(cityInput)
         });
     }
-}
+};
 
 // EVENT LISTENERS
 $("#searchButton").on("click", function (event) {
     event.preventDefault();
 
     var cityInput = $("#searchBar").val().trim();
-    
+
     cityArray.push(cityInput);
     localStorage.setItem("cityData", JSON.stringify(cityArray))
     renderSideBar();
